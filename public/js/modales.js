03f1f519a01af58d30729cdfc891e71013044fe2
isenv <isenv.projects@outlook.com>
2020-12-06 11:55:24 -0500

    login y registro de usuario

--- public/js/modales.js
+++ public/js/modales.js
@@ -1,3 +1,17 @@
-function mostar_modal(dom_modal) {
-    alert(dom_modal)
+document.getElementById('modal_inicioSesion').style.display = 'flex'
+document.getElementById('modal_registroUsuSesion').style.display = 'none'
+
+function mostar_modal(modal_registroUsuSesion, modal_inicioSesion) {
+
+    let display_ini = modal_inicioSesion.style.display
+    let display_reg = modal_registroUsuSesion.style.display
+
+    if (display_ini == 'flex') {
+        modal_inicioSesion.style.display = 'none'
+        modal_registroUsuSesion.style.display = 'flex'
+    }
+    if (display_reg == 'flex') {
+        modal_inicioSesion.style.display = 'flex'
+        modal_registroUsuSesion.style.display = 'none'
+    }
 }
